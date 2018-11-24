8908b770624390684cd9cc305c33a8b5635ac5d9
Chris Hayes <chrisroberthayes@gmail.com>
2018-11-24 18:03:37 -0500

    Add an example that uses .then rather than async/await

---
+++ 6-2/app.js
@@ -0,0 +1,64 @@
+var App = (function() {
+  function App() {}
+
+  App.prototype.init = function() {
+    this.fetchTracks('rock').then(tracks => {
+      this.render(tracks);
+    });
+  }
+
+  App.prototype.onGenreChange = function(event) {
+    this.fetchTracks(event.value).then(tracks => {
+      this.render(tracks);
+    });
+  }
+
+  App.prototype.fetchTracks = function(genreSlug) {
+    const genreSlugToIdMap = {
+      'rock': 7,
+      'pop': 8,
+      'hiphop': 9,
+      'soul': 10,
+    };
+
+    const genreId = genreSlugToIdMap[genreSlug];
+
+    return $.ajax({
+      url: 'https://api.musixmatch.com/ws/1.1/track.search',
+      method: 'GET',
+      dataType: 'jsonp',
+      data: {
+        apikey: '3ab5684680f38c7ad48e5a3d9078a81c',
+        format: 'jsonp',
+        f_music_genre_id: genreId,
+        page_size: '5',
+        page: 1,
+        s_track_rating: 'DESC'
+      },
+    }).then(res => {
+      return res.message.body.track_list.map(item => {
+        return {
+          name: item.track.album_name,
+        }
+      });
+    });
+  }
+
+  App.prototype.render = function(tracks) {
+    const $trackListContainer = $('.track-list');
+
+    $trackListContainer.empty();
+
+    tracks.forEach(track => {
+      $trackListContainer.append(`
+        <div class="list-group-item">${track.name}</div>
+      `);
+    })
+  }
+
+  return App;
+}());
+
+var app = new App();
+
+app.init();
